cf66992f5658d419f8870a03bbe033e62381714e
Julia <earnkay2@gmail.com>
2022-11-09 20:51:26 +0100

    button toggling

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,18 @@
 
-        const sideBar = document.getElementById('side_nav');
-             const sideBtn = document.getElementById('sidebtn');
+        const sideBar = document.getElementById('side_nav'),
+              sideBtn = document.getElementById('sidebtn'),
+              sideimg = document.querySelector('.sideimg')
 
         sideBtn.addEventListener('click', opensidebar);
 
-        function opensidebar(e){
+        function opensidebar(){
+            // e.preventDefault();
             console.log('this is a click event listener');
             if (sideBar.style.display === 'none') { 
                 sideBar.style.display = 'block'
+                sideimg.style.display = 'none'
             } else {
                 sideBar.style.display = 'none'
+                sideimg.style.display = 'block'
             }
         }
